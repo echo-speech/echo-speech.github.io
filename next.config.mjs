b569d8f9d2d11b8ce627d7bee9521713a269bb9d
IndiaInk10 <indiaink10@gmail.com>
2025-02-08 23:29:51 +0900

    Mod gitignore and Add SSG

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   basePath: "/echo-speech.github.io",
-  assetPrefix: "/echo-speech.github.io",
+  assetPrefix: "/echo-speech.github.io/",
 
   async headers() {
     return [
